6136ffa4251c329a2c2b4adbb1d38a939938e82f
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2011-03-22 19:17:29 +0000

    Use correct global and local work sizes

--- benchmarks/cp/src/klee/main.cpp
+++ benchmarks/cp/src/klee/main.cpp
@@ -9,9 +9,10 @@
 
 int main(int argc, char **argv) {
   unsigned numatoms = 2;
-  voldim3i grid = { 16, 16, 1 };
+  voldim3i grid = { 32, 32, 1 };
   size_t volsize[3] = { grid.x, grid.y, grid.z };
-  size_t localWorkSize[3] = { grid.x/2, grid.y/2, 1 };
+  size_t globalWorkSize[3] = { grid.x/UNROLLX, grid.y, grid.z };
+  size_t localWorkSize[3] = { globalWorkSize[0]/2, globalWorkSize[1]/2, 1 };
 
   float gridspacing;
   float *atoms = (float *) malloc(4*numatoms * sizeof(float));
@@ -21,7 +22,7 @@ int main(int argc, char **argv) {
   klee_make_symbolic(atoms, 4*numatoms * sizeof(float), "atoms");
 
   cpuenergy(grid, numatoms, gridspacing, 0, atoms, cenergy);
-  gpuenergy(volsize, volsize, localWorkSize, numatoms, gridspacing, atoms, genergy);
+  gpuenergy(volsize, globalWorkSize, localWorkSize, numatoms, gridspacing, atoms, genergy);
   
   int same;
   for (int x = 0; x < grid.x * grid.y * grid.z; ++x) {
